1c1b6067e4b492c618aaca10b6fedbe4e7811a9a
wymstar <wymstar@naver.com>
2016-10-10 01:16:46 +0900

    chap11-2

--- chap11/socket.io.server.js
+++ chap11/socket.io.server.js
@@ -13,5 +13,9 @@ var server = http.createServer(function(request, response) {
 
 var io = socketio.listen(server);
 io.sockets.on('connection', function(socket){
+  socket.on('rint', function(data) {
+    console.log('Client Send Data:' , data);
 
+    socket.emit('smart', data);
+  });
 });
