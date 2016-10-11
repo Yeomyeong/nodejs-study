9f5d16a8db248b51dff88610f03866a75c4fa2cf
wymstar <wymstar@naver.com>
2016-10-11 23:58:00 +0900

    chap11

--- chap11/socket.io.server.js
+++ chap11/socket.io.server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var socketio = require('socket.io');
 
 var server = http.createServer(function(request, response) {
-  fs.readFile('HTMLPage.html', function(error, data){
+  fs.readFile('basic.html', function(error, data){
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(data);
   });
@@ -11,11 +11,22 @@ var server = http.createServer(function(request, response) {
   console.log('Server running at http://127.0.0.1:52273');
 });
 
+var id = 0;
 var io = socketio.listen(server);
 io.sockets.on('connection', function(socket){
+  id = socket.id;
   socket.on('rint', function(data) {
     console.log('Client Send Data:' , data);
 
     socket.emit('smart', data);
+
+    //퍼블릭 통신
+    //io.socket.emit('smart', data);
+
+    //broadcast 통신
+    //socket.broadcast.emit('smart', data);
+
+    //private 통신
+    //io.socket.to(id).emit('smart', data);
   });
 });
